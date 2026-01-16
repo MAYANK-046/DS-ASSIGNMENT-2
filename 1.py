0acd34f6ba96437a0b642ec96ae0dceb936e5880
MAYANK KUMAR SINGH <mayank.25162@ee.du.ac.in>
2026-01-16 10:33:23 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,28 @@
+"""
+Problem statement 1 - Arranging Clothes in an Almirah.
+
+Step1 - Start
+Step2 - First we will take input:-
+        a) Number of shelves (n).
+        b) Listing of clothing items with categories like formal wear, casual wear, traditional wear, party wear for a week.
+
+Step3 - Now we will create separate groups for each category.
+
+Step4 - Identifying clothes by how often we use them:-
+        In this step we will identify frequently used clothes and occasionally used clothes.
+        a) Clothes we use most often are like casual wear, formal wear.
+        b) Clothes we use least often are like traditional attire, party wear.
+
+Step5 - Sorting clothes by their usage:-
+        a) We will put clothes we use most often at first.
+        b) And we will put clothes we use least often at last.
+
+Step6 - Assigning shelves to clothes:-
+        a) By placing clothes we use most often on middle shelves like easy to reach and minimum search time.
+        b) By placing clothes we use least often on top or bottom shelves.
+
+Step7 - Within each self we will keep clothes by folding it uniformly to save space.
+
+Step8 - End
+
+"""
---
+++ 2.py
@@ -0,0 +1,34 @@
+"""
+Problem statement 2 - Arranging Clothes in a Luggage Bag.
+
+Step 1 - Start
+Step 2 - First we will take input:-
+        a) Trip duration which is 7 days.
+        b) Activities planned like sightseeing, hiking, formal evening dinners.
+        c) List of clothing items and accessories.
+
+Step 3 - We will categorize items into:- 
+        a) Heavy clothes like jeans, jackets.
+        b) Light clothes like t-shirts, innerwear.
+        c) Delicate clothes like formal wears.
+        d) Accessories like belts, socks, toiletries.
+
+Step 4 - We will roll daily wear clothes such as undergarments instead of folding to save space.
+
+Step 5 - Placing items in the luggage:-
+        a) Put heavy and bulky items at the bottom.
+        b) Place daily-wear clothes in the middle.
+        c) Place frequently needed items on top.
+
+Step 6 - Protect delicate clothing by wrapping them in plastic covers or placing between soft clothes.
+
+Step 7 - Using small spaces to put small things like:-
+        a) Filling shoes with socks or accessories.
+        b) Using side pockets for chargers and toiletries.
+
+Step 8 - Place nightwear or a change of clothes on top for easy access after travel.
+
+Step 9 - Close luggage and check space utilization.
+
+Step 10 - End
+"""
